29d6dff73872ef53307931fafd4896b94d26afe7
Alok Singh <allstaralok@gmail.com>
2020-04-16 12:15:00 +0530

    v

--- src/test/java/com/showtime/service/application/management/ApplicationManagementApplicationTests.java
+++ src/test/java/com/showtime/service/application/management/ApplicationManagementApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ApplicationManagementApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	//@Test
+	//void contextLoads() {
+	//}
 
 }
